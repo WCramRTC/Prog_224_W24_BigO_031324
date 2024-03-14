46f0c6fc1d064b302d6dd9df353def6eaee251ff
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-03-13 18:16:05 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Prog_224_W24_BigO_031324
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Big O Notation
+            //  Constant: O(1)
+
+            int add = 2 + 1;
+
+        //  Linear time: O(n)
+        //  Logarithmic time: O(n log n)
+        //  Quadratic time: O(n ^ 2)
+        //  Exponential time: O(2 ^ n)
+        //  Factorial time: O(n!)
+
+        } // Main
+
+
+    } // class
+
+} // namespace
